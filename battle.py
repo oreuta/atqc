671e30be1a60bca6a8b8bf366c65223f0ecb4fe7
antondugin <darkmesia@mail.ru>
2017-03-15 21:18:35 +0200

    wait+Elf

--- battle.py
+++ battle.py
@@ -1,3 +1,5 @@
+'''Warrior'''
+
 class Warrior():
     '''
     Warrior is a base class for all wariors in the Battle.
@@ -58,12 +60,45 @@ class Warrior():
             self.__strength,
             self.__health
         ))
-        
+
+    def wait(self):
+        self.__strength = round(self.__strength * 1.1)
+        self.__health = round(self.__health * 1.1)
+        self.make_sound("Master, I feel good! My strength is {} and health is {}".format(
+            self.__strength,
+            self.__health
+        ))
   
+'''Exp'''
+
+class Exp():
+    def __init__(self):
+       self.__exp = 0  
+    def get__exp(self):
+       return self.__exp
+    def set__exp(self):
+       if self.__win == True:
+           self.__exp = self._exp + 1
+       else:
+           self.__exp = self.__exp
 
 
+'''Elf'''
 
-
+class Elf(Warrior):
+    def __init__(self, name, hsfactor = 0.3):
+        self.__name = name.upper()
+        self.__health = round(Warrior.get_power() * hsfactor)
+        self.__strength = round(Warrior.get_power() * (1 - hsfactor))
+        power = self.__health + self.__strength # must be 1 after rounding
+        if power > 1:
+            self.__health - 1
+        elif power < 1:
+            self.__health + 1
+        self.make_sound("I've born! My strenght is {} and health is {}".format(
+            round(self.__strength),
+            round(self.__health)
+        ))
     
 
 
